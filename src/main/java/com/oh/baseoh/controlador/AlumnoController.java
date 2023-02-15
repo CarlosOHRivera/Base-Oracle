d2f56c6e79cf380d9bebc2338f7b1ef149f6e297
Carlos OH Rivera <cariveh@gmail.com>
2023-02-15 12:43:01 -0600

    Se agregaron las opciones de nuevo alumno al controlador con sus respectivas html

--- src/main/java/com/oh/baseoh/controlador/AlumnoController.java
+++ src/main/java/com/oh/baseoh/controlador/AlumnoController.java
@@ -1,11 +1,14 @@
 package com.oh.baseoh.controlador;
 
 import com.oh.baseoh.modelo.Alumno;
+import com.oh.baseoh.repositorio.AlumnoRepositorio;
 import com.oh.baseoh.servicio.AlumnoServicioImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,6 +19,21 @@ import java.util.List;
 public class AlumnoController {
     @Autowired
     AlumnoServicioImpl alumnoServicioimpl;
+    @Autowired
+    AlumnoRepositorio alumnoRepositorio;
+    @GetMapping("/nuevo")
+    public String nuevoAlumno(Model model){
+        Alumno alumno = new Alumno();
+        model.addAttribute("alumnos" ,alumno );
+        return "view-alumno";
+
+    }
+
+    @PostMapping("/guardar")
+    public String guardarAlumno(@ModelAttribute("alumnos") Alumno htmlalumno ){
+        alumnoRepositorio.save(htmlalumno);
+        return "redirect:/alumnos/lista";
+    }
 
     @GetMapping("/lista")
     public String AlumnosList(Model model) {
